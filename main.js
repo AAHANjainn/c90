8f3001a90634e73844cd12ccd3b3d5d0a2bbf9b9
AAHANjainn <92387845+AAHANjainn@users.noreply.github.com>
2022-06-08 11:24:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+
+function login()
+{
+  name_1 = document.getElementById("username_1").value;
+  name_2 = document.getElementById("username_2").value;
+  document.getElementById("name_1").innerHTML = name_1
+  document.getElementById("name_2").innerHTML = name_2;
+  window.location="screen2.html";
+}
