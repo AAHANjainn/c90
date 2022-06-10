ae5de53917fb12478d169e0987f24ed35ff63b04
AAHANjainn <92387845+AAHANjainn@users.noreply.github.com>
2022-06-10 10:46:09 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 
+
 function login()
 {
   name_1 = document.getElementById("username_1").value;
   name_2 = document.getElementById("username_2").value;
-  document.getElementById("name_1").innerHTML = name_1
-  document.getElementById("name_2").innerHTML = name_2;
+  localStorage.setItem("name_1" , name_1);
+  localStorage.setItem("name_2" , name_2);
+  diplay_name_1 = localStorage.getItem("name_1");
+  diplay_name_2 = localStorage.getItem("name_2");
+  document.getElementById("name_1").innerHTML=diplay_name_1;
   window.location="screen2.html";
 }
